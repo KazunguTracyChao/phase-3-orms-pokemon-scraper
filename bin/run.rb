a143721555fdd77fa553633cd3240b943d47a847
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-05-27 23:02:51 +0300

    Completed assignment

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,11 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+
+all_pokemon.each do |row|
+    id = row[0]
+    name = row[1]
+    type = row[2]
+    puts "Pokemon ID: #{id}, Name: #{name}, Type: #{type}"
+  end
+  
